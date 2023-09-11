19a64adba588b2c5d7488738de406f431ace50a7
Patrick <patricknaashat@yahoo.com>
2023-09-11 21:46:41 +0300

    add calculator brain

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct CalculatorBrain {
+    
+    var bmi: Float?
+
+    mutating func calculateBMI(height: Float, weight: Float) {
+        bmi = weight / pow(height, 2)
+    }
+    
+    func getBMIValue() -> String {
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi ?? 0.0)
+        return bmiTo1DecimalPlace
+    }
+}
